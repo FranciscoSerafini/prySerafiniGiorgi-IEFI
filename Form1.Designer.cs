d7add9afe32fd9b0920672a93fab51c3c1065731
FranciscoSerafini <franciscoserafinigiorgi@gmail.com>
2022-10-26 23:48:59 -0300

    Agregado de formularios

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.sistemasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.agregarNuevosClientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultaClienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modificarClienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarClienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.listadoDeClieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listadoDeSociosDeudoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sistemasToolStripMenuItem,
+            this.clientesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(649, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // sistemasToolStripMenuItem
+            // 
+            this.sistemasToolStripMenuItem.Name = "sistemasToolStripMenuItem";
+            this.sistemasToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.sistemasToolStripMenuItem.Text = "Sistemas";
+            // 
+            // clientesToolStripMenuItem
+            // 
+            this.clientesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.agregarNuevosClientesToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.consultaClienteToolStripMenuItem,
+            this.modificarClienteToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.eliminarClienteToolStripMenuItem,
+            this.listadoDeClieToolStripMenuItem,
+            this.listadoDeSociosDeudoresToolStripMenuItem});
+            this.clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.clientesToolStripMenuItem.Text = "Clientes";
+            // 
+            // agregarNuevosClientesToolStripMenuItem
+            // 
+            this.agregarNuevosClientesToolStripMenuItem.Name = "agregarNuevosClientesToolStripMenuItem";
+            this.agregarNuevosClientesToolStripMenuItem.Size = new System.Drawing.Size(222, 22);
+            this.agregarNuevosClientesToolStripMenuItem.Text = "Agregar nuevos clientes";
+            // 
+            // consultaClienteToolStripMenuItem
+            // 
+            this.consultaClienteToolStripMenuItem.Name = "consultaClienteToolStripMenuItem";
+            this.consultaClienteToolStripMenuItem.Size = new System.Drawing.Size(222, 22);
+            this.consultaClienteToolStripMenuItem.Text = "Buscar socio";
+            // 
+            // modificarClienteToolStripMenuItem
+            // 
+            this.modificarClienteToolStripMenuItem.Name = "modificarClienteToolStripMenuItem";
+            this.modificarClienteToolStripMenuItem.Size = new System.Drawing.Size(222, 22);
+            this.modificarClienteToolStripMenuItem.Text = "Modificar socio";
+            // 
+            // eliminarClienteToolStripMenuItem
+            // 
+            this.eliminarClienteToolStripMenuItem.Name = "eliminarClienteToolStripMenuItem";
+            this.eliminarClienteToolStripMenuItem.Size = new System.Drawing.Size(223, 22);
+            this.eliminarClienteToolStripMenuItem.Text = "Listado de todos los clientes";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(219, 6);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(219, 6);
+            // 
+            // listadoDeClieToolStripMenuItem
+            // 
+            this.listadoDeClieToolStripMenuItem.Name = "listadoDeClieToolStripMenuItem";
+            this.listadoDeClieToolStripMenuItem.Size = new System.Drawing.Size(222, 22);
+            this.listadoDeClieToolStripMenuItem.Text = "Listado de todos los socios";
+            // 
+            // listadoDeSociosDeudoresToolStripMenuItem
+            // 
+            this.listadoDeSociosDeudoresToolStripMenuItem.Name = "listadoDeSociosDeudoresToolStripMenuItem";
+            this.listadoDeSociosDeudoresToolStripMenuItem.Size = new System.Drawing.Size(222, 22);
+            this.listadoDeSociosDeudoresToolStripMenuItem.Text = "Listado de socios deudores";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(649, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sistemasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agregarNuevosClientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem consultaClienteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modificarClienteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem eliminarClienteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listadoDeClieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listadoDeSociosDeudoresToolStripMenuItem;
     }
 }
 
